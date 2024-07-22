dd51514a65e1e7e881b30026710227b124702ffc
Karol Cywinski <karolsky02@gmail.com>
2024-07-22 08:56:19 +0200

    Add admin authorization

--- main.py
+++ main.py
@@ -47,78 +47,21 @@ async def db_lifespan(app: FastAPI):
             app.mongodb_client.close()
 
 app = FastAPI(lifespan=db_lifespan)
-
-# Requests
-
-# Custom exception for database server errors
-db_server_exception = HTTPException(
-    status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-    detail="Internal Server Error (unable to connect to database)"
-)
-
-@app.get(
-    "/users", 
-    response_model=list[models.User], 
-    response_model_by_alias=False
-)
-async def get_all_users():
-    try:
-        return await crud.get_all_users(app.mongodb_db)
-    except ServerSelectionTimeoutError:
-        raise db_server_exception     
-    except:
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail="Internal Server Error (unable to read users)"
-        )
-
-@app.post(
-    "/users", 
-    response_model=models.User, 
-    response_model_by_alias=False,
-    status_code=status.HTTP_201_CREATED,
-    response_description="New user created"
-)
-async def create_user(user: models.UserCreate):
-    try:
-        db_user = await crud.get_user(app.mongodb_db, user.username)
-    except ServerSelectionTimeoutError:
-        raise db_server_exception 
-    except:
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail="Internal Server Error (unable to verify username)"
-        )        
-    else:
-        if db_user:
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST, 
-                detail="User with that name already exists"
-            )
-        try:
-            return await crud.create_user(app.mongodb_db, user)
-        except ServerSelectionTimeoutError:
-            raise db_server_exception 
-        except:
-            raise HTTPException(
-                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                detail="Internal Server Error (unable to create user)"
-            ) 
-             
+            
 # Authentication
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-def create_jwt_token(user_id: str):
+def create_jwt_token(user_id: str, admin: bool):
     expire = datetime.now(timezone.utc) + timedelta(minutes=JWT_EXPIRE_MINUTES)
-    payload_to_encode = {"sub": user_id, "exp": expire}
+    payload_to_encode = {"sub": user_id, "exp": expire, "admin": admin}
     return jwt.encode(payload_to_encode, JWT_SECRET_KEY, algorithm=JWT_ALGORITHM)
 
-async def get_current_user(token: str = Depends(oauth2_scheme)):
+async def get_current_user(token: str = Depends(oauth2_scheme), only_admin: bool = False):
     try:
         payload = jwt.decode(token, JWT_SECRET_KEY, algorithms=[JWT_ALGORITHM])
         user_id = payload.get("sub")
-        return user_id
+        is_admin = payload.get("admin")
     except ExpiredSignatureError:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
@@ -131,6 +74,19 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
             detail="Could not validate credentials",
             headers={"WWW-Authenticate": "Bearer"},
         )
+    else:
+        if only_admin and not is_admin:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="Admin credentials required",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+            
+        return user_id
+
+    
+async def get_current_admin_user(token: str = Depends(oauth2_scheme)):
+    return await get_current_user(token, only_admin=True)
 
 @app.post("/token", response_model=models.Token)
 async def get_access_token(login_data: OAuth2PasswordRequestForm = Depends()):
@@ -154,11 +110,68 @@ async def get_access_token(login_data: OAuth2PasswordRequestForm = Depends()):
         else:
             # Verify if login password matches password stored in database
             if bcrypt.checkpw(login_data.password.encode("utf-8"), db_user["hashed_password"]):
-                jwt_encoded_token = create_jwt_token(str(db_user["_id"]))
+                jwt_encoded_token = create_jwt_token(str(db_user["_id"]), db_user["is_admin"])
                 return models.Token(access_token=jwt_encoded_token, token_type="bearer")
             else:
                 raise user_login_exception
             
+# Requests
+
+# Custom exception for database server errors
+db_server_exception = HTTPException(
+    status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+    detail="Internal Server Error (unable to connect to database)"
+)
+
+@app.get(
+    "/users", 
+    response_model=list[models.User], 
+    response_model_by_alias=False
+)
+async def get_all_users(current_admin_user_id: str = Depends(get_current_admin_user)):
+    try:
+        return await crud.get_all_users(app.mongodb_db)
+    except ServerSelectionTimeoutError:
+        raise db_server_exception     
+    except:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Internal Server Error (unable to read users)"
+        )
+
+@app.post(
+    "/users", 
+    response_model=models.User, 
+    response_model_by_alias=False,
+    status_code=status.HTTP_201_CREATED,
+    response_description="New user created"
+)
+async def create_user(user: models.UserCreate, current_admin_user_id: str = Depends(get_current_admin_user)):
+    try:
+        db_user = await crud.get_user(app.mongodb_db, user.username)
+    except ServerSelectionTimeoutError:
+        raise db_server_exception 
+    except:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Internal Server Error (unable to verify username)"
+        )        
+    else:
+        if db_user:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST, 
+                detail="User with that name already exists"
+            )
+        try:
+            return await crud.create_user(app.mongodb_db, user)
+        except ServerSelectionTimeoutError:
+            raise db_server_exception 
+        except:
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="Internal Server Error (unable to create user)"
+            )
+
 @app.get("/test-auth")
 async def test_auth(current_user_id: str = Depends(get_current_user)):
     return {"text": f"User with id {current_user_id} authenticated :)"}
